153544544a5f02f7589d128fef34a2a065b6f2ed
Joshua <joshua.soave@gmail.com>
2023-04-04 08:50:14 -0400

    adding build

---
+++ build/precache-manifest.1624dc3a2af971dee60b10d9a03b8f0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63afe28f02c86d912e2f",
+    "url": "/static/css/main.54922dbe.chunk.css"
+  },
+  {
+    "revision": "63afe28f02c86d912e2f",
+    "url": "/static/js/main.63afe28f.chunk.js"
+  },
+  {
+    "revision": "8b75a05db659c745233e",
+    "url": "/static/js/1.8b75a05d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84a12b198f7c23d01bc60e54013c6a6b",
+    "url": "/index.html"
+  }
+];
